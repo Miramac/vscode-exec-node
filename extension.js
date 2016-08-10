13fa9b38a830b3a4ed88cbd7456618230b8eb541
Fabian Roloff <fabian.roloff@vocatus.de>
2016-08-10 12:34:38 +0200

    cleanup

--- extension.js
+++ extension.js
@@ -1,6 +1,5 @@
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 exports.activate = require('./lib/activate')
 
 // this method is called when your extension is deactivated
