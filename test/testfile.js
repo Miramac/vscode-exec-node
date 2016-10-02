988b5744251043a2531e55e9924d3d14d980cc8f
Fabian Roloff <fabian.roloff@vocatus.de>
2016-10-02 12:08:34 +0200

    format fix

--- test/testfile.js
+++ test/testfile.js
@@ -1,8 +1,8 @@
-//Press F8 to start and F9 to cancel the process 
-function endlessCount(i) {
+// Press F8 to start and F9 to cancel the process
+function endlessCount (i) {
   console.log(i)
-  setTimeout(()=> {
-      endlessCount(++i)
-  },1000)
+  setTimeout(() => {
+    endlessCount(++i)
+  }, 1000)
 }
 endlessCount(1)
