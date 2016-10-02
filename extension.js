4797da2bdfb071bfc7a52bb07f8eea17ed0b191d
Fabian Roloff <fabian.roloff@vocatus.de>
2016-10-02 12:08:19 +0200

    version up

--- extension.js
+++ extension.js
@@ -4,5 +4,6 @@ exports.activate = require('./lib/activate')
 
 // this method is called when your extension is deactivated
 function deactivate () {
+  // add some cleanup
 }
 exports.deactivate = deactivate
