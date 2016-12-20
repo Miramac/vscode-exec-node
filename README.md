e8a630393e59f308ab470b9f9ad040b565f5064f
roloff <fabian.roloff@vocatus.de>
2016-12-20 18:53:43 +0100

    new mode: use integrated terminal

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Node Exec
-### Execute your current file or your selection with node.js.
+### Execute the current file or your selected code with node.js.
 
 ## Usage
 * To execute the current file or the selection press `F8` or use the command `Execute Node.js`
--- test/testfile.js
+++ test/testfile.js
@@ -1,4 +1,6 @@
 // Press F8 to start and F9 to cancel the process
+console.log(__dirname)
+console.log('\x1b[36m', 'sometext' ,'\x1b[0m');
 function endlessCount (i) {
   console.log(i)
   setTimeout(() => {
@@ -6,3 +8,4 @@ function endlessCount (i) {
   }, 1000)
 }
 endlessCount(1)
+
