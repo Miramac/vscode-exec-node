fcd0634e831f9bb48b41ef5c98761975b0b9e827
Fabian Roloff <fabian.roloff@vocatus.de>
2017-02-24 14:04:44 +0100

    update testfile

--- test/testfile.js
+++ test/testfile.js
@@ -1,11 +1,15 @@
 // Press F8 to start and F9 to cancel the process
 console.log(__dirname)
-console.log('\x1b[36m', 'sometext' ,'\x1b[0m');
+console.log('\x1b[36m', 'sometext', '\x1b[0m')
 function endlessCount (i) {
   console.log(i)
   setTimeout(() => {
     endlessCount(++i)
   }, 1000)
 }
+
+console.log(process.env.NODE_ENV)
+console.log(process.argv)
+
 endlessCount(1)
 
